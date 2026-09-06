860ba94b89a376986fd67ba2caabe2bbad48dad4
shiba-rose <shemyakinnikolka@gmail.com>
2026-09-06 22:58:40 +0300

    test5

---
+++ lesson.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+//include " std liЬ facilities . h"
+using namespace std; //        
+int main() // Программы на С++ на чинаются с вылолнения функции ma in 
+{
+    cout << "Hello, World!\n" ; // Вывод строки "Hello, World!"
+    //keep_window_open ( ) ; // Ожидание нажатия клавиши перед закрытием окна консоли,но нам это не надо 
+    return 0;
+}
